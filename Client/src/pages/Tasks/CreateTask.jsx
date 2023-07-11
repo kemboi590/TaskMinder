412160966e61b111409a013e3a7a6d33bfedec4b
kemboi590 <bkemboi590@gmail.com>
2023-07-11 12:03:47 +0300

    finished create task page

--- Client/src/pages/Tasks/CreateTask.jsx
+++ Client/src/pages/Tasks/CreateTask.jsx
@@ -1,58 +1,113 @@
 import React from "react";
+import "./createtask.css";
 
 function CreateTask() {
   return (
     <div className="create_task_page">
-      <h2>Create Task</h2>
-      <div className="task_form">
-        <form action="">
-          <label htmlFor="title">Title</label>
+      <form>
+        <div className="task_form">
+          <div>
+            <label className="task_title"> Task Title</label>
+            <br />
+            <input
+              className="title_input"
+              type="tect"
+              placeholder="your task title"
+              // {...register("Email")}
+            />
+          </div>
           <br />
-          <input
-            className="title_input"
-            type="tect"
-            placeholder="task title"
-            // {...register("Email")}
-          />
+          <div>
+            <label className="task_description">Description</label>
+            <br />
+            <textarea
+              className="description_input"
+              cols="30"
+              rows="10"
+              placeholder=" your task description"
+              // {...register("Email")}
+            ></textarea>
+          </div>
           <br />
-          <label htmlFor="description">Description</label>
-          <br />
-          <textarea
-            className="title_input"
-            cols="30"
-            rows="10"
-            // {...register("Email")}
-          ></textarea>
-          <br />
-          <label htmlFor="status">Status</label>
-          <br />
-          {/* create a check box with a lable assign task to: */}
-          <label htmlFor="status">Assign Task To:</label>
-
-          <input type="checkbox" id="" value="Kemboi" />
-          <label htmlFor="">kemboi</label>
+          <div>
+            <label className="task_assign_to">Assign Task To</label>
+            <br />
+            <div className="check_member">
+              <input type="checkbox" id="" value="Kemboi" />
+              <label classname="assignee">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label classname="assignee">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <input type="checkbox" id="" value="Kemboi" />
+              <label htmlFor="">kemboi</label>
+              <br />
+              <input type="checkbox" id="" value="Kemboi" />
+              <label classname="assignee">kemboi</label>
+              <br />
+            </div>
+          </div>
 
           <br />
           {/* calender to choose due date */}
-          <label htmlFor="dueDate">Due Date</label>
-          <br />
-          <input type="date" name="dueDate" id="dueDate" />
+          <div>
+            <label className="task_dueDate">Due Date</label>
+            <br />
+            <input
+              type="date"
+              name="dueDate"
+              className="dueDate_calender"
+              // {...register("Email")}
+            />
+          </div>
           <br />
           {/* set priority by using radio butons*/}
-          <label htmlFor="priority">Priority</label>
-          <br />
-          <input type="radio" name="priority" id="priority" />
-          <label htmlFor="priority">High</label>
-          <input type="radio" name="priority" id="priority" />
-          <label htmlFor="priority">Medium</label>
-          <input type="radio" name="priority" id="priority" />
-          <label htmlFor="priority">Low</label>
+          <div>
+            <label className="task_priority">Priority</label>
+            <br />
+            <div className="radio_task">
+              <input
+                type="radio"
+                className="radio_priority"
+                // {...register("Email")}
+              />
+              <label className="task_priority">High</label>
+              <input
+                type="radio"
+                className="radio_priority"
+                // {...register("Email")}
+              />
+              <label className="task_priority">Medium</label>
+              <input
+                type="radio"
+                className="radio_priority"
+                // {...register("Email")}
+              />
+              <label className="priority">Low</label>
+            </div>
+          </div>
           <br />
 
           {/* a submit button saying create task */}
-          <input type="submit" value="Create Task" className="submit_btn" />
-        </form>
-      </div>
+          <input type="submit" value="Create Task" className="submit_task" />
+        </div>
+      </form>
     </div>
   );
 }
