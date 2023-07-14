fff0b3080766cab444cc5d1f9e615209c29d90d1
kemboi590 <bkemboi590@gmail.com>
2023-07-14 11:13:24 +0300

    done with viewsinlge task

--- Client/src/pages/Tasks/SingleTask.jsx
+++ Client/src/pages/Tasks/SingleTask.jsx
@@ -30,52 +30,84 @@ function SingleTask() {
     fetchSingleTask();
   }, [id]);
 
+  const haddleClose = () => {
+    navigate("/tasks");
+  };
+
   return (
     <div className="task_page">
       <table className="single_task_table">
+        <caption className="single_task_title">SINGLE TASK</caption>
         <tbody>
-          {task.map((task) => (
-            <React.Fragment key={task.id}>
-              <tr>
-                <td>Title:</td>
-                <td>{task.title}</td>
-              </tr>
-              <tr>
-                <td>Description:</td>
-                <td>{task.description}</td>
-              </tr>
-              <tr>
-                <td>Priority:</td>
-                <td>{task.priority}</td>
-              </tr>
-              <tr>
-                <td>Due Date:</td>
-                <td>{task.due_date}</td>
-              </tr>
-              <tr>
-                <td>Status:</td>
-                <td>Not done</td>
-              </tr>
-              <tr>
-                <td>Assigned To:</td>
-                <td>{task.username}</td>
-              </tr>
-              <tr>
-                <td>Created At:</td>
-                <td>{task.created_at}</td>
-              </tr>
-              <tr>
-                <td>Email:</td>
-                <td>{task.email}</td>
-              </tr>
-              <tr>
-                <td>Task ID:</td>
-                <td>{task.task_id}</td>
-              </tr>
-            </React.Fragment>
-          ))}
+          {task.map((task) => {
+            // for created at
+            const createdAt = new Date(task.created_at);
+            const CreatedDate = createdAt.toDateString();
+            const TimeCreated = createdAt.toLocaleTimeString();
+
+            // for due date
+            const dueDate = new Date(task.due_date);
+            const dueDateDate = dueDate.toDateString();
+            const dueDateTime = dueDate.toLocaleTimeString();
+            return (
+              <React.Fragment key={task.id}>
+                <tr>
+                  <td>Title:</td>
+                  <td>{task.title}</td>
+                </tr>
+                <tr>
+                  <td>Description:</td>
+                  <td>{task.description}</td>
+                </tr>
+
+                <tr>
+                  <td>Created At:</td>
+                  <td>
+                    {CreatedDate} {TimeCreated}
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>Due Date:</td>
+                  <td>
+                    {dueDateDate} {dueDateTime}
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>Assigned To:</td>
+                  <td>{task.username}</td>
+                </tr>
+
+                <tr>
+                  <td>Email:</td>
+                  <td>{task.email}</td>
+                </tr>
+
+                <tr>
+                  <td>Priority:</td>
+                  <td>{task.priority}</td>
+                </tr>
+
+                <tr>
+                  <td>Status:</td>
+                  <td>Not Yet started</td>
+                </tr>
+
+                <tr>
+                  <td>Task ID:</td>
+                  <td>{task.task_id}</td>
+                </tr>
+              </React.Fragment>
+            );
+          })}
         </tbody>
       </table>
+      <div className="single_task_buttons">
+        <button onClick={haddleClose}> Close </button>
+        <button> Update </button>
+        <button> Delete </button>
+      </div>
     </div>
   );
 }
